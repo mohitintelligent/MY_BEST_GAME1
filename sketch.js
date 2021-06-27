21a69e01b47690bb1c6d527c90c86445ceb2d459
mohitintelligent <74311289+mohitintelligent@users.noreply.github.com>
2021-06-27 12:10:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+const Matter = matter.js;
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+
+var engine, world;
+var box1, pig1;
+var backgroundImg,platform;
+var bird, slingShot;
+
+function preload(){
+
+}
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+function draw(){
+    background("pink");
+    Engine.update(engine);
+
+    drawWprites();
+}
